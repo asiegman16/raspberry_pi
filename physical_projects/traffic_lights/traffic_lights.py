086c7e7c8ad069566d0fd2ca9de1159101b3c6af
asiegman16 <asiegman16@gmail.com>
2020-04-29 12:06:04 -0400

    update traffic_lights

--- physical_projects/traffic_lights/traffic_lights.py
+++ physical_projects/traffic_lights/traffic_lights.py
@@ -1,4 +1,4 @@
-from gpiozero import Button, LED
+"""from gpiozero import Button, LED
 
 button = Button(21) 
 led = LED(25)
@@ -7,25 +7,31 @@ while True:
 	button.wait_for_press()
 	led.on() # or, led.blink() or, led.blink(2,2) which means 2 seconds on, 2 seconds off
 	button.wait_for_release()
-	led.off()
+	led.off()"""
 
 
-# /// # /// # /// BELOW IS FULL TRAFFIC LIGHT CODE # /// # /// # ///
-
-"""from gpiozero import Button, TrafficLights, Buzzer
+from gpiozero import Button, Buzzer, LED
 from time import sleep 
 
-lights = TrafficLights(25,8,7)
+red = LED(25)
+amber = LED(8)
+green = LED(7)
 buzzer = Buzzer(15)
 
 while True: 
 	button.wait_for_press()
-	lights.green.on()
+	red.on
+	buzzer.beep(.01,1)
 	sleep(1)
-	lights.amber.on()
+	amber.on()
+	buzzer.beep(.01,1)
 	sleep(1)
-	lights.red.on()
+	green.on()
+	buzzer.beep(.01,1)
+	buzzer.off()
 	sleep(1)
-	lights.off()"""
+	red.off()
+	amber.off()
+	green.off()
 	
 
